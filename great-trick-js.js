d96be28a18410fba41db06721ccf62f65a03aafc
mishalov <forwow495800@ya.ru>
2019-07-17 15:51:58 +0500

    Update great-trick-js.js

--- great-trick-js.js
+++ great-trick-js.js
@@ -1,21 +1,23 @@
 var handler = {
-  get: function(target, name, reciever) {
-    const res = [];
-    for(let i = 0; i < reciever; i++) {
-      const func = eval(`${name}`);
-      res.push(func(i));
-      
+    get: function(target, name, reciever) {
+      let func;
+      try {
+        func = eval(${name});
+      } catch (e) {
+        if (e instanceof ReferenceError) return undefined;
+        console.log("e: ", e);
+      }
+      const res = [];
+      for (let i = 0; i < reciever; i++) {
+        res.push(func(i));
+      }
+      return res;
     }
-    return res
-  }
-}
-var p = new Proxy(Object, handler);
-Object.setPrototypeOf(Number.prototype, p);
-
-console.log(5[x => (5-x)])
-
-
-console.log(3[x => x])
-
-
-console.log(1[x => 5])
+  };
+  var p = new Proxy(Object, handler);
+  Object.setPrototypeOf(Number.prototype, p);
+  console.log(5[x => (5-x)])
+  console.log(3[x => x])
+  console.log(1[x => 5])
+  
+  
